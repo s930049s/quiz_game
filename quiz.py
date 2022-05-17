ed6a8ffea859d3d176680ca57e75521402e7c681
DKDLP <s930049s@gmail.com>
2022-05-17 15:51:01 +0800

    v1.1 - add count

--- quiz.py
+++ quiz.py
@@ -1,8 +1,10 @@
 import random
 num = random.randint(1, 100)
-
+count = 0
 while True:
     num_guess = int(input("輸入一個數字: "))
+    count = count +1
+    print ("猜了", count, "次")
     if num_guess > num: 
         print("比答案大")
     elif num_guess < num:
